f980e45c24c03abc843938ad9a78931fb7f30ad6
revanthragiri039 <revanthragiri039@gmail.com>
2022-08-10 13:22:24 +0530

    Regration changes.

--- src/app/loan-dashboard/loan-dashboard.component.ts
+++ src/app/loan-dashboard/loan-dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, resolveForwardRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoanServiceService } from '../loan-service.service';
-import { FormGroup,FormControl,Validators,FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { SearchModel } from '../Models/SearchModel';
 @Component({
@@ -10,102 +10,98 @@ import { SearchModel } from '../Models/SearchModel';
   styleUrls: ['./loan-dashboard.component.css']
 })
 export class LoanDashboardComponent implements OnInit {
-  searchloandetails:FormGroup;
-  loanlist:any=[];
-  todisplay=false;
-  public errMsg:any;
-  loandetailsresponse:any;
-  role:any="";
-  constructor(private _loanservice:LoanServiceService,private _router:Router,private _frombuilder:FormBuilder) 
-  { 
-    this.searchloandetails=this._frombuilder.group({
-      firstname:[],
-      lastname:[],
-      loannumber:[]
+  searchloandetails: FormGroup;
+  loanlist: any = [];
+  todisplay = false;
+  public errMsg: any;
+  loandetailsresponse: any;
+  role: any = "";
+  showorhidetable: boolean = false;
+  constructor(private _loanservice: LoanServiceService, private _router: Router, private _frombuilder: FormBuilder) {
+    this.searchloandetails = this._frombuilder.group({
+      firstname: [],
+      lastname: [],
+      loannumber: []
     });
   }
 
   ngOnInit() {
-    this.role=localStorage.getItem('Role');
-    if(this.role=="User")
-    {
-      this.todisplay=true;
+    this.role = localStorage.getItem('Role');
+    if (this.role == "User") {
+      this.todisplay = true;
     }
-     this.loanlist.data=0;
+    this.loanlist.data = 0;
   }
 
-  GetLoansList()
-  {
-    const model= new SearchModel("","","");
-    this._loanservice.getLoans(model).subscribe(a=>{    
-      this.loanlist=a;
-    },(error)=>{
-      Swal.fire("Loan Dashboard!!",error,'error');
-      this.loanlist.data=0;
+  GetLoansList() {
+    const model = new SearchModel("", "", "");
+    this._loanservice.getLoans(model).subscribe(a => {
+      this.loanlist = a;
+    }, (error) => {
+      Swal.fire("Loan Dashboard!!", error, 'error');
+      this.loanlist.data = 0;
     });
   }
 
-  EditLoan(Id:any)
-  { 
-   this._router.navigate(['/updateLoan',Id]);
+  EditLoan(Id: any) {
+    this._router.navigate(['/updateLoan', Id]);
   }
 
-  DeleteLoan(Id:any)
-  {
+  DeleteLoan(Id: any) {
     Swal.fire({
-      title:'Are you sure want to delete?',
-      text :'Loan Details!!',
-      icon :'warning',
-      showCancelButton:true,
-      confirmButtonText:'Yes, delete it!',
-      cancelButtonText:'Cancel'
-    }).then((response:any)=>{
-      if(response.value)
-      {
-        this._loanservice.DeleteLoanbyId(Id).subscribe(x=>{
-          this.loandetailsresponse =x;
-          console.log(this.loandetailsresponse,'Test');
-          if(this.loandetailsresponse.success==true)
-          {
-            Swal.fire('Deleted!','Loan details have been deleted successfully.','success')
+      title: 'Are you sure want to delete?',
+      text: 'Loan Details!!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'Cancel'
+    }).then((response: any) => {
+      if (response.value) {
+        this._loanservice.DeleteLoanbyId(Id).subscribe(x => {
+          this.loandetailsresponse = x;
+          console.log(this.loandetailsresponse, 'Test');
+          if (this.loandetailsresponse.success == true) {
+            Swal.fire('Deleted!', 'Loan details have been deleted successfully.', 'success')
             //this.GetLoansList();
-            this.loanlist.data=0;
+            this.loanlist.data = 0;
+            this.showorhidetable = false;
           }
-          else{
-            Swal.fire('Deleted!',this.loandetailsresponse.data.message,'error')
+          else {
+            Swal.fire('Deleted!', this.loandetailsresponse.data.message, 'error')
+            this.showorhidetable = false;
           }
-        },(error)=>{
-          Swal.fire("Loan Dashboard!!",error,'error');
+        }, (error) => {
+          Swal.fire("Loan Dashboard!!", error, 'error');
         });
-        
+
       }
     })
   }
 
-  AddnewLoan()
-  {
+  AddnewLoan() {
     this._router.navigate(['/addLoan'])
   }
 
-  searchLoandetails(from:any)
-  {
-    const model= new SearchModel(
-      this.searchloandetails.value.firstname==null?"":this.searchloandetails.value.firstname,
-      this.searchloandetails.value.lastname==null?"":this.searchloandetails.value.lastname,
-      this.searchloandetails.value.loannumber==null?"":this.searchloandetails.value.loannumber
-      );
-    this._loanservice.getLoans(model).subscribe(a=>{    
-      this.loanlist=a;
-    },(error)=>{
-      Swal.fire('Loan Dashboard!!',error,'error');
-      this.loanlist.data=0;
+  searchLoandetails(from: any) {
+    const model = new SearchModel(
+      this.searchloandetails.value.firstname == null ? "" : this.searchloandetails.value.firstname,
+      this.searchloandetails.value.lastname == null ? "" : this.searchloandetails.value.lastname,
+      this.searchloandetails.value.loannumber == null ? "" : this.searchloandetails.value.loannumber
+    );
+    this._loanservice.getLoans(model).subscribe(a => {
+      this.loanlist = a;
+      this.showorhidetable = true;
+    }, (error) => {
+      Swal.fire('Loan Dashboard!!', error, 'error');
+      this.loanlist.data = 0;
+      this.showorhidetable = false;
     });
   }
 
-  Clear()
-  {
+  Clear() {
     this.searchloandetails.reset({});
-    this.loanlist.data=0;
+    this.loanlist.data = 0;
+    this.showorhidetable = false;
   }
 
 }
